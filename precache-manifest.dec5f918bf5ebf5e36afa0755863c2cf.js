7b825bad340c3c19abaa1bf5394511f54c0badb7
Abhishek Thory <abhithory@gmail.com>
2021-10-31 20:33:16 +0530

    Updates

---
+++ precache-manifest.dec5f918bf5ebf5e36afa0755863c2cf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "8554e726c62ed25f1f57",
+    "url": "/nft_website/static/css/main.23531fd2.chunk.css"
+  },
+  {
+    "revision": "8554e726c62ed25f1f57",
+    "url": "/nft_website/static/js/main.8554e726.chunk.js"
+  },
+  {
+    "revision": "c81f5d7edd9425b15936",
+    "url": "/nft_website/static/css/1.cd397256.chunk.css"
+  },
+  {
+    "revision": "c81f5d7edd9425b15936",
+    "url": "/nft_website/static/js/1.c81f5d7e.chunk.js"
+  },
+  {
+    "revision": "3875d8fd60dae1baa678",
+    "url": "/nft_website/static/js/runtime~main.3875d8fd.js"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/nft_website/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/nft_website/static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "ea9ed37af3e95dd1629a9581ceb2c4ed",
+    "url": "/nft_website/static/media/Itim-Regular.ea9ed37a.ttf"
+  },
+  {
+    "revision": "468e9e481b5dc25887b7604dc7aef41a",
+    "url": "/nft_website/index.html"
+  }
+];
