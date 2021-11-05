0c8de0760bc8c3cfd910317eccc1bd312f955e67
Abhishek Thory <abhithory@gmail.com>
2021-11-05 14:48:07 +0530

    Updates

---
+++ precache-manifest.29d626c7f7ba7cfdede50ba9dc059eb7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae4badb39ec9239c69cf",
+    "url": "/nft_website/static/css/main.8e0225a9.chunk.css"
+  },
+  {
+    "revision": "ae4badb39ec9239c69cf",
+    "url": "/nft_website/static/js/main.ae4badb3.chunk.js"
+  },
+  {
+    "revision": "bfaa38ad4eb5d2ea48fc",
+    "url": "/nft_website/static/css/1.844944cd.chunk.css"
+  },
+  {
+    "revision": "bfaa38ad4eb5d2ea48fc",
+    "url": "/nft_website/static/js/1.bfaa38ad.chunk.js"
+  },
+  {
+    "revision": "3875d8fd60dae1baa678",
+    "url": "/nft_website/static/js/runtime~main.3875d8fd.js"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/nft_website/static/media/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/nft_website/static/media/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "c93b702953b665a168d30ac1f03ee21c",
+    "url": "/nft_website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/nft_website/precache-manifest.2a40255d74bcac206cdccb186a077cce.js"
+  "/nft_website/precache-manifest.29d626c7f7ba7cfdede50ba9dc059eb7.js"
 );
 
 workbox.clientsClaim();
